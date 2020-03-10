98989634d97bf0de11a7cf8c1a78d4727adaf4d1
JoshuaFarmerATX <53586634+JoshuaFarmerATX@users.noreply.github.com>
2020-03-10 18:15:02 -0500

    Final version

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -4,7 +4,6 @@ const input = d3.select("input")
 const button = d3.select("button")
 
 const table = d3.select("#ufo-table")
-console.log(table)
 table.html("")
 
 const thead = table.append("thead")
@@ -29,36 +28,67 @@ data.forEach(record => {
     row.append("td").text(record.duration)
     row.append("td").text(record.comments)
 })
+
+
 const handler = function(){
 
     let filterCond = input.property("value")
 
-    const table = d3.select("#ufo-table")
-    table.html("")
+    if (filterCond === "") {
+        
+        table.html("")
 
-    const thead = table.append("thead")
-    const head_row = thead.append("tr")
-    const tbody = table.append("tbody")
+        const thead = table.append("thead")
+        const head_row = thead.append("tr")
+        const tbody = table.append("tbody")
 
-    head_row.append("th").text("Date")
-    head_row.append("th").text("City")
-    head_row.append("th").text("State")
-    head_row.append("th").text("Country")
-    head_row.append("th").text("Shape")
-    head_row.append("th").text("Duration")
-    head_row.append("th").text("Comments")
+        head_row.append("th").text("Date")
+        head_row.append("th").text("City")
+        head_row.append("th").text("State")
+        head_row.append("th").text("Country")
+        head_row.append("th").text("Shape")
+        head_row.append("th").text("Duration")
+        head_row.append("th").text("Comments")
 
-    data.filter(record => record.datetime === filterCond).forEach(record => {
-        let row = tbody.append("tr")
-        row.append("td").text(record.datetime)
-        row.append("td").text(record.city)
-        row.append("td").text(record.state)
-        row.append("td").text(record.country)
-        row.append("td").text(record.shape)
-        row.append("td").text(record.duration)
-        row.append("td").text(record.comments)
+        data.forEach(record => {
+            let row = tbody.append("tr")
+            row.append("td").text(record.datetime)
+            row.append("td").text(record.city)
+            row.append("td").text(record.state)
+            row.append("td").text(record.country)
+            row.append("td").text(record.shape)
+            row.append("td").text(record.duration)
+            row.append("td").text(record.comments)
+        })
+    } else {
 
-    })
+        const table = d3.select("#ufo-table")
+        table.html("")
+
+        const thead = table.append("thead")
+        const head_row = thead.append("tr")
+        const tbody = table.append("tbody")
+
+        head_row.append("th").text("Date")
+        head_row.append("th").text("City")
+        head_row.append("th").text("State")
+        head_row.append("th").text("Country")
+        head_row.append("th").text("Shape")
+        head_row.append("th").text("Duration")
+        head_row.append("th").text("Comments")
+
+        data.filter(record => record.datetime === filterCond).forEach(record => {
+            let row = tbody.append("tr")
+            row.append("td").text(record.datetime)
+            row.append("td").text(record.city)
+            row.append("td").text(record.state)
+            row.append("td").text(record.country)
+            row.append("td").text(record.shape)
+            row.append("td").text(record.duration)
+            row.append("td").text(record.comments)
+
+        })
+    }
 }
 
 input.on("change", handler)
